4bc95acaa6fab50bc2e2c7c58e957b5eeb277c23
Emanuel.ps <emanuelpages.ps@gmail.com>
2023-05-19 17:25:57 -0300

    modificaciones en about me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -31,16 +31,15 @@ export default function AboutMe(scrollAboutMe) {
         <motion.section animate={animation} className="box">
           <h1>About Me</h1>
           <p>
-            I am a passionate junior front-end developer with a desire to learn
+            I am a passionate Full Stack developer with aspiration to learn
             and grow in a collaborative work environment and team. I have
-            knowledge in HTML, CSS, JavaScript, and ReactJS. Currently, I am
-            still learning and taking back-end programming courses at
-            Coderhouse. My future goal is to become a full-stack programmer. I
+            knowledge in HTML, CSS, JavaScript, ReactJS. Currently, I am
+            still learning and taking the back-end development courses at
+            Coderhouse. My future goal is to become a experiencied full-stack developer. I
             have been working in Digital Marketing for more than 6 years in
-            different companies, both in technology ecommerce companies and
-            digital marketing agencies. My desire is to be able to get my first
-            job in the programming industry and develop myself professionally as
-            a programmer.
+            different companies, both related with E-commerce and Digital Marketing agencies. My goal is to be able to get my first
+            job in the programming industry and evolve myself professionally as
+            a developer.
           </p>
         </motion.section>
     </div>
