dd4c8228a188b2b254f0590688a8e84851eb665d
emanuelpps <emanuelpages.ps@gmail.com>
2023-07-14 16:37:41 -0300

    correcciones en links componente contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Contact.css";
 import Form from "react-bootstrap/Form";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
@@ -9,12 +9,30 @@ import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 import { useInView } from "react-intersection-observer";
 import { useAnimation, motion } from "framer-motion";
+import { Popover } from "react-tiny-popover";
 
 export default function Contact() {
-  
   const { ref, inView } = useInView();
   const animation = useAnimation();
   const animationTwo = useAnimation();
+
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
+  const getMailCopy = () => {
+    navigator.clipboard.writeText("emanuelpages.ps@gmail.com");
+    setIsPopoverOpen(!isPopoverOpen);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsPopoverOpen(false);
+    }, 2000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isPopoverOpen]);
+
   const email = "emanuelpages.ps@gmail.com";
 
   useEffect(() => {
@@ -24,7 +42,7 @@ export default function Contact() {
         transition: {
           type: "spring",
           duration: 2,
-          bounce: 0
+          bounce: 0,
         },
       });
       animationTwo.start({
@@ -32,15 +50,14 @@ export default function Contact() {
         transition: {
           type: "spring",
           duration: 2,
-          bounce: 0
-        },        
-      })
+          bounce: 0,
+        },
+      });
     }
     if (!inView) {
       animation.start({ x: "-100vw" });
       animationTwo.start({ x: "100vw" });
     }
-
 
     console.log("this is in view", inView);
   }, [animation, inView]);
@@ -85,35 +102,57 @@ export default function Contact() {
       </div>
       <div ref={ref} className="form-contact">
         <motion.div animate={animation} className="form">
-          <form className="form-data"  onSubmit={sendEmail}>
+          <form className="form-data" onSubmit={sendEmail}>
             <Form.Group className="mb-5" controlId="exampleForm.ControlInput1">
-              <Form.Label for="from_name" className="text-input" name="from_name"
-              id="from_name"
-              required>Name</Form.Label>
+              <Form.Label
+                for="from_name"
+                className="text-input"
+                name="from_name"
+                id="from_name"
+                required
+              >
+                Name
+              </Form.Label>
               <Form.Control type="email" placeholder="name" />
             </Form.Group>
             <Form.Group className="mb-5" controlId="exampleForm.ControlInput1">
-              <Form.Label type="email" name="reply_to"
-              id="reply_to" className="text-input" for="reply_to" 
-              required >Email address</Form.Label>
+              <Form.Label
+                type="email"
+                name="reply_to"
+                id="reply_to"
+                className="text-input"
+                for="reply_to"
+                required
+              >
+                Email address
+              </Form.Label>
               <Form.Control type="email" placeholder="name@example.com" />
             </Form.Group>
             <Form.Group
               className="mb-5"
               controlId="exampleForm.ControlTextarea1"
             >
-              <Form.Label for="message" className="text-input" name="message"
-              id="message">Message</Form.Label>
+              <Form.Label
+                for="message"
+                className="text-input"
+                name="message"
+                id="message"
+              >
+                Message
+              </Form.Label>
               <Form.Control as="textarea" rows={6} />
             </Form.Group>
             <button class="btn btn-outline-light" type="submit">
-                Send
+              Send
             </button>
           </form>
         </motion.div>
         <motion.div animate={animationTwo} className="info">
           <ul className="info-data">
-            <Link className="text-link">
+            <Link
+              className="text-link"
+              to={"https://www.linkedin.com/in/emanuel-p"}
+            >
               <button>
                 <div className="icon-contact">
                   <FaLinkedinIn />
@@ -121,22 +160,26 @@ export default function Contact() {
                 Linkedin
               </button>
             </Link>
-            <Link className="text-link">
-            <button>
-              <div className="icon-contact">
-                <FaGithub />
-              </div>
-              Github
-            </button>
+            <Link className="text-link" to={"https://github.com/emanuelpps"}>
+              <button>
+                <div className="icon-contact">
+                  <FaGithub />
+                </div>
+                Github
+              </button>
             </Link>
-            <Link className="text-link">
-            <button>
-              <div className="icon-contact">
-                <AiOutlineMail />
-              </div>
-              emanuelpages.ps@gmail.com
-            </button>
-            </Link>
+            <Popover
+              isOpen={isPopoverOpen}
+              positions={["bottom"]}
+              content={<div className="popover text-center">Mail Copiado!</div>}
+            >
+              <button id="Mail" onClick={() => getMailCopy()}>
+                <div className="icon-contact">
+                  <AiOutlineMail />
+                </div>
+                emanuelpages.ps@gmail.com
+              </button>
+            </Popover>
           </ul>
         </motion.div>
       </div>
