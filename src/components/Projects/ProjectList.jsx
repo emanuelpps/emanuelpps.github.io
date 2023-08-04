ca9b9fec11553035e682530b738be66741d9ba42
emanuelpps <emanuelpages.ps@gmail.com>
2023-08-04 10:14:17 -0300

    correccion ancho de seccion about en version responsive

--- src/components/Projects/ProjectList.jsx
+++ src/components/Projects/ProjectList.jsx
@@ -3,11 +3,11 @@ import ProjectsCards from "../../assets/data/Projects.json";
 import ProjectCard from "./ProjectCard";
 
 export default function ProjectList() {
-    return (
-        <div className="project-list">
-          {ProjectsCards.map((project, index) => (
-              <ProjectCard  className="projects-card" key={index} project={project}/>
-          ))}
-        </div>
-      );
-    }
+  return (
+    <div className="project-list">
+      {ProjectsCards.map((project, index) => (
+        <ProjectCard className="projects-card" key={index} project={project} />
+      ))}
+    </div>
+  );
+}
